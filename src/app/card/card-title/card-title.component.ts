c0f45227ba38fb04a9886102f1a2aa926b774995
juliocsalves <juliocsalves@gmail.com>
2025-07-07 00:00:35 -0300

    correção do dropdown

--- src/app/card/card-title/card-title.component.ts
+++ src/app/card/card-title/card-title.component.ts
@@ -2,22 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-card-title',
-  standalone: true,
-  imports: [
-		NgIf
-	],
-  templateUrl: './card-title.component.html',
-  styleUrls: ['./card-title.component.css']
+	selector: 'app-card-title',
+	standalone: true,
+	imports: [NgIf],
+	templateUrl: './card-title.component.html',
+	styleUrls: ['./card-title.component.css']
 })
 export class CardTitleComponent implements OnInit {
-  show = false;
+	show = false;
 	cartCount = 0;
+	categoriasAtivas = false;
+	categoriasVisiveis = false;
 
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.show = true;
-			this.cartCount = 3; /* aqui vai a contagem de itens, se for zero o cartCount fica hidden no html */
-    }, 100);
-  }
+	ngOnInit(): void {
+		setTimeout(() => {
+			this.show = true;
+			this.cartCount = 3;
+		}, 100);
+	}
+
+	toggleCategorias() {
+		if (this.categoriasAtivas) {
+			this.categoriasAtivas = false;
+			setTimeout(() => this.categoriasVisiveis = false, 400); // igual ao tempo da transição
+		} else {
+			this.categoriasVisiveis = true;
+			setTimeout(() => this.categoriasAtivas = true, 10); // ligeiro delay pra iniciar transição
+		}
+	}
 }
+
